e9ea37a01fd4a878fea10fa536cdb2cfc064bcd3
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2025-12-18 13:01:15 +0300

    update to angular21 & primeng21

--- primeng-quickstart-tailwind/src/app/app.config.ts
+++ primeng-quickstart-tailwind/src/app/app.config.ts
@@ -1,20 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideAnimationsAsync(),
     providePrimeNG({
       theme: {
         preset: Aura,
         options: { darkModeSelector: '.p-dark' },
       },
     }),
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZonelessChangeDetection(),
     provideRouter(routes),
   ],
 };
